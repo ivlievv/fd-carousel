f5a9bcc0980b54bb3a65c6057209d4d23f8bccec
User Name <artur.it2914@gmail.com>
2019-12-17 15:16:04 +0200

    Create: Carousel class

--- assets/js/carousel/index.js
+++ assets/js/carousel/index.js
@@ -1,7 +1,74 @@
 'use strict';
 
+class Carousel {
+  /**
+   *
+   * @param {Array<string>} slidesPaths
+   */
+  constructor (slidesPaths) {
+    if(slidesPaths.length){
+      this._slides = slidesPaths;
+      this._currentIndex = 0
+    }
+    else{
+      throw new Error()
+    }
 
+  }
 
+  get currentIndex(){
+    return this._currentIndex;
+  }
 
+  /**
+   *
+   * @param {number} value
+   */
+  set currentIndex(value){
+    if(isNaN(value)){
+      throw new TypeError();
+    }
+    if (value < 0 || value > this._slides.length - 1) {
+      throw new RangeError();
+    }
+     this._currentIndex = value;
+  }
 
-export default {};
+  goNext () {
+    this.currentIndex = Carousel.getNextIndex(this.currentIndex, this._slides.length);
+  }
+
+  goPrev () {
+    this.currentIndex = Carousel.getPrevIndex(this.currentIndex, this._slides.length);
+  }
+
+  /**
+   *
+   * @param {number} index
+   * @param {number} length
+   * @return {number} - next slide index in carousel
+   * @static
+   */
+  static getNextIndex (index, length) {
+    if (index < 0 || index > length - 1) {
+      throw new RangeError();
+    }
+    return (index + 1) % length;
+  }
+
+  /**
+   *
+   * @param {number} index
+   * @param {number} length
+   * @return {number} - previous slide index in carousel
+   * @static
+   */
+  static getPrevIndex (index, length) {
+    if (index < 0 || index > length - 1) {
+      throw new RangeError();
+    }
+    return (index - 1 + length) % length;
+  }
+}
+
+export default Carousel;
