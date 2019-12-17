d9c6d180a3da9d67c7aac7e785181b62643b18a7
User Name <artur.it2914@gmail.com>
2019-12-17 16:12:44 +0200

    Create: carousel view

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,30 @@
 'use strict';
 
-import carousel from './carousel/index.js'
+import Carousel from './carousel/index.js';
 
+class CarouselController {
+  constructor (slidesPath) {
+    this._slidesPath = slidesPath;
+    this._isFetching = true;
+    this._carousel = null;
+    this.loadSlides();
+  }
 
+  get isFetching(){
+    return this._isFetching;
+  }
 
+  loadSlides () {
+    fetchJson(this._slidesPath)
+      .then(data => {
+        this._carousel = new Carousel(data);
+        this._isFetching = false;
+      });
+  }
+
+}
+
+async function fetchJson (url, options) {
+  const response = await fetch(url, options);
+  return await response.json();
+}
