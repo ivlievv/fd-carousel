5b98477a271b398a0d3ef654f99fbd27b0280eb7
User Name <artur.it2914@gmail.com>
2019-12-18 15:28:48 +0200

    Create: very bad carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,39 @@
 
 import Carousel from './carousel/index.js';
 
-class CarouselController{
+const CAROUSEL_ID = 'CAROUSEL_ID';
+
+class CarouselController {
+  /**
+   *
+   * @param {string} slidesPath
+   */
   constructor (slidesPath) {
     this._slidesPath = slidesPath;
-    this._isFetching = true;
+    this._isFetching = false;
     this._carousel = null;
+    this._error = null;
     this.onload = null;
-    this.loadSlides = this.loadSlides.bind(this);
-    setTimeout(this.loadSlides, 4000)
+    this.loadSlides();
+    /*
+     this.loadSlides2 = this.loadSlides2.bind(this);
+     */
+  }
+
+  get error () {
+    return this._error;
+  }
+
+  set error (value) {
+    if (value instanceof Error) {
+      this._error = value;
+      if (typeof this.onerror === 'function') {
+        this.onerror(new Event('error'));
+      }
+      this.isFetching = false;
+    } else {
+      throw new TypeError();
+    }
   }
 
   get isFetching () {
@@ -21,18 +46,37 @@ class CarouselController{
    * @param {boolean} value
    */
   set isFetching (value) {
+    if (typeof value !== 'boolean') {
+      throw new TypeError();
+    }
     this._isFetching = value;
-    if(!value && typeof this.onload === 'function'){
-      this.onload()
+    if (value && typeof this.onstart === 'function') {
+      this.onstart();
+    }
+    if (!value && !this.error && typeof this.onload === 'function') {
+      this.onload();
     }
   }
-   loadSlides () {
+
+  /*  loadSlides2(){
+   fetchJson(this._slidesPath)
+   .then(data => {
+   this._carousel = new Carousel(data);
+   this.isFetching = false;
+   });
+   };*/
+  loadSlides = () => {
+
+    this.isFetching = true;
     fetchJson(this._slidesPath)
       .then(data => {
         this._carousel = new Carousel(data);
         this.isFetching = false;
+      })
+      .catch(err => {
+        this.error = err;
       });
-  }
+  };
 
   get carousel () {
     return this._carousel;
@@ -60,76 +104,109 @@ class CarouselController{
   }
 
   renderSlideContainer () {
+
     const container = document.createElement('div');
     container.classList.add('slidesContainer');
 
-    this.carousel.slides.forEach((value, index) => {
-      container.appendChild(this.renderSlide(value, index));
-    });
+    if (this.isFetching) {
+
+      const spinner = document.createElement('h1');
+      spinner.innerText = 'Loading...';
+      container.appendChild(spinner);
+
+    } else if (this.error) {
+
+    } else {
+      this.carousel.slides.forEach((value, index) => {
+        container.appendChild(this.renderSlide(value, index));
+      });
+    }
 
     return container;
   }
 
-  renderButton(){
+  renderButton () {
     const button = document.createElement('div');
     button.classList.add('button');
+    const arrowLeftImage = new Image();
+    arrowLeftImage.src = `${location.origin}/assets/icons/arrow.png`;
+    button.appendChild(arrowLeftImage);
     return button;
   }
 
-  goNext(){
-    
-  }
+  goNext = () => {
 
-  goPrev(){
-    
-  }
-  
+    const currentIndex = this._carousel.currentIndex;
+
+    const prevSlide = document.getElementById(Carousel.getPrevIndex(currentIndex, this._carousel.length));
+    const currentSlide = document.getElementById(this._carousel.currentIndex);
+    const nextSlide = document.getElementById(Carousel.getNextIndex(currentIndex, this._carousel.length));
+    const newNext = document.getElementById(
+      Carousel.getNextIndex(Carousel.getNextIndex(currentIndex, this._carousel.length), this._carousel.length));
+
+    prevSlide.classList.remove('prevSlide');
+    currentSlide.classList.replace('currentSlide', 'prevSlide');
+    nextSlide.classList.replace('nextSlide', 'currentSlide');
+    newNext.classList.add('nextSlide');
+    this._carousel.goNext();
+  };
+
+  goPrev = () => {
+
+  };
+
   render () {
 
-    if(this.isFetching){
-      const spinner = document.createElement('h1');
-      spinner.innerText = "Loading...";
-      return spinner;
-    }else{
+    const prevButton = this.renderButton();
+    prevButton.addEventListener('click', this.goPrev);
 
-      const prevButton = this.renderButton();
-      const arrowLeftImage = new Image();
-      arrowLeftImage.src = `${location.origin}/assets/icons/arrow-left.png`;
-      prevButton.appendChild(arrowLeftImage);
-      prevButton.addEventListener('click', this.goPrev);
+    const nextButton = this.renderButton();
+    nextButton.style.transform = 'rotate(180deg)';
+    nextButton.style.transformOrigin = 'center';
+    nextButton.addEventListener('click', this.goNext);
 
+    const carouselWrapper = document.createElement('div');
+    carouselWrapper.classList.add('carouselWrapper');
+    carouselWrapper.appendChild(prevButton);
+    carouselWrapper.appendChild(this.renderSlideContainer());
+    carouselWrapper.appendChild(nextButton);
 
-      const nextButton = this.renderButton();
-      const arrowRightImage = new Image();
-      arrowRightImage.src = `${location.origin}/assets/icons/arrow-right.png`;
-      nextButton.appendChild(arrowRightImage);
-      nextButton.addEventListener('click', this.goNext);
-
-      const carouselWrapper = document.createElement('div');
-      carouselWrapper.classList.add('carouselWrapper');
-      carouselWrapper.appendChild(prevButton);
-      carouselWrapper.appendChild( this.renderSlideContainer() );
-      carouselWrapper.appendChild(nextButton);
-
-      return carouselWrapper;
-    }
+    return carouselWrapper;
 
   }
 
 }
 
 async function fetchJson (url, options) {
+
   const response = await fetch(url, options);
   return await response.json();
+
 }
 
 const carouselController = new CarouselController('./images.json');
 
-const firstRenderElem = carouselController.render();
-document.body.appendChild( firstRenderElem );
+const newCarousel = carouselController.render();
+newCarousel.setAttribute('id', CAROUSEL_ID);
+document.body.appendChild(newCarousel);
+carouselController.onstart = () => {
+  reRenderCarousel();
+};
 
 carouselController.onload = () => {
-  document.body.replaceChild( carouselController.render(),firstRenderElem );
+  reRenderCarousel();
 };
+
+carouselController.onerror = () => {
+  reRenderCarousel();
+};
+
+function reRenderCarousel () {
+  const newCarousel = carouselController.render();
+  newCarousel.setAttribute('id', CAROUSEL_ID);
+  document.body.replaceChild(newCarousel, document.getElementById(CAROUSEL_ID));
+}
+
+
 
 
